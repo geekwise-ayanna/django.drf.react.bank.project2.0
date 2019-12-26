0b436b2447e6fe55c211aba6699ace16c7293fec
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2019-12-26 11:49:21 -0800

    bugfix: changed import code

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Bank.models import Account, Branch, Customer, Product
+from main.models import Account, Branch, Customer, Product
 
 # Register your models here.
 admin.site.register(Account)
