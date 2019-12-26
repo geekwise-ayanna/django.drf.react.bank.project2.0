8517fdb58ea148aeea52f417e126ffe2ac1f2b88
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2019-12-26 11:47:20 -0800

    added: registered models in `admin.py` file

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Bank.models import Account, Branch, Customer, Product
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Branch)
+admin.site.register(Customer)
+admin.site.register(Product)
