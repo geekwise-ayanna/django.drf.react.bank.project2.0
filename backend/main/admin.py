026fb812596ce2b1a6f6670bdfcd338386fd7c86
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2019-12-26 13:42:55 -0800

    bugfix: fixed import on line 2 (possibly still getting an indentation error)

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import Account, Branch, Customer, Product
+from models import Account, Branch, Customer, Product
 
 # Register your models here.
 admin.site.register(Account)
