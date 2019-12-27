95740d986239185c5cdbd22129121409e072002e
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2019-12-26 16:51:45 -0800

    added: urls to `urls.py`

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from bank import views as bank_views
+
+router = routers.DefaultRouter()
+router.register(r'branch', bank_views.Branch_Viewset)
+router.register(r'customer', bank_views.Customer_Viewset)
+router.register(r'account', bank_views.Account_Viewset)
+router.register(r'product', bank_views.Product_Viewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
