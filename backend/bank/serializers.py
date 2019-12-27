e98d16c78d373d641021912873380d1762e3f481
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2019-12-26 16:33:12 -0800

    added: serializers to `serializers.py` and updated customer first name and last name in `models.py`

---
+++ backend/bank/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Account, Branch, Customer, Product
+
+
+class Branch_Serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Branch
+        fields = [
+            'id',
+            'location_name',
+            'location'
+        ]
+
+
+class Customer_Serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = [
+            'id',
+            'customer_first_name',
+            'customer_last_name',
+        ]
+
+
+class Account_Serializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Account
+        fields = [
+            'id',
+            'account_name',
+            'account_no',
+            'account_type',
+            'account_balance',
+        ]
+
+
+class Product_Serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'loan_type',
+            'loan_balance',
+            'amount_due',
+            'date_due',
+        ]
