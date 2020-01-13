af2e19963d2edd7d2c2930796171685697ce3351
geekwise-ayanna <geekwise.ayanna.bs@gmail.com>
2020-01-13 15:38:14 -0800

    added: user authentication and tokens

--- react-login-signup-ui-template/src/components/login.component.js
+++ react-login-signup-ui-template/src/components/login.component.js
@@ -1,19 +1,57 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: {
+                'username': "",
+                'password': ""
+            }
+        };
+    }
+
+    onSubmit() {
+        console.log(this.state.activeItem)
+        Axios
+            .post("http://localhost:8000/users/api/auth/login", this.state.activeItem)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
+    }
+
+    handleChange = e => {
+        let { name, value} = e.target;
+        const activeItem = { ...this.state.activeItem, [name]: value};
+        this.setState({ activeItem });
+    };
+
     render() {
         return (
             <form>
                 <h3>Sign In</h3>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>User Name</label>
+                    <input
+                        type="text"
+                        name="username"
+                        className="form-control"
+                        placeholder="Enter user name"
+                        value={this.state.activeItem.username}
+                        onChange={this.handleChange}
+                     />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                        name="password"
+                        value={this.state.activeItem.password}
+                        onChange={this.handleChange}/>
                 </div>
 
                 <div className="form-group">
@@ -23,7 +61,7 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="button" className="btn btn-primary btn-block" onClick={() => this.onSubmit()}>Submit</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
--- react-login-signup-ui-template/src/components/signup.component.js
+++ react-login-signup-ui-template/src/components/signup.component.js
@@ -1,32 +1,73 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: {
+                'username': "",
+                'email': "",
+                'password': ""
+            }
+        };
+    }
+
+    onSubmit() {
+        console.log(this.state.activeItem)
+        Axios
+            .post("http://localhost:8000/users/api/auth/register", this.state.activeItem)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
+    }
+
+    handleChange = e => {
+        let { name, value } = e.target;
+        const activeItem = { ...this.state.activeItem, [name]: value };
+        this.setState({ activeItem });
+    };
+
     render() {
         return (
             <form>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <label>User Name</label>
+                    <input
+                        type="text"
+                        name="username"
+                        className="form-control"
+                        placeholder="Enter user name"
+                        value={this.state.activeItem.username}
+                        onChange={this.handleChange}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                        value={this.state.activeItem.email}
+                        onChange={this.handleChange}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                        name="password"
+                        value={this.state.activeItem.password}
+                        onChange={this.handleChange} />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="button" className="btn btn-primary btn-block" onClick={() => this.onSubmit()} >Sign Up</button>
                 <p className="forgot-password text-right">
                     Already registered? <a href="#">Sign in here.</a>
                 </p>
